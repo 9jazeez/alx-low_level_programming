350b78711e5424f701d124a98c5fc1b18d83888c
9jazeez <9jazeez@gmail.com>
2021-06-18 06:35:52 -0700

    Corrected more betty styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- *
  * main - Entry point
  *
  *Return: Always 0 (Success)
  */
-
-int main(void)
-{
-puts("\"Programming is like building amultilingual puzzle");
-
-return (0);
-}
+	int main(void)
+		{
+			puts("\"Programming is like building a multiligual puzzle");
+			return (0);
+		}
