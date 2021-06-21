28830b7955432eba8e552831f9bb7554dfbc1369
9jazeez <9jazeez@gmail.com>
2021-06-21 07:47:08 -0700

    Advance task correction made

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 	int i = 0;
-	char c[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char c[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	while (i < 58)
 	{
 		putchar(c[i]);
 		i++;
 	}
+	putchar('\n');
 	return (1);
 }
