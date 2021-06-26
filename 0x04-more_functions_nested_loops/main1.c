2892dad04d4a07d8bfe8d934e0d5b88d5e2e0135
9jazeez <9jazeez@gmail.com>
2021-06-26 21:45:02 +0000

    Print Triangle alternative

---
+++ 0x04-more_functions_nested_loops/main1.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	print_triangle(0);
+	print_triangle(1);
+	print_triangle(24);
+	print_triangle(20);
+	print_triangle(10);
+
+	return (0);
+}
