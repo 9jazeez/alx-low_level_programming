3ebd9cd4f009c585fc6d0d846941dee8719d08ae
9jazeez <9jazeez@gmail.com>
2021-07-09 12:37:36 +0000

    Task 1 completed

---
+++ 0x09-static_libraries/holberton.h
@@ -0,0 +1,41 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *, char *);
+char *cap_string(char *);
+void _putchar(int);
+int _strlen(char *);
+int _strncmp(char *, char *, int );
+int _strlen_recursion(char *);
+int factorial(int n);
+int _pow_recursion(int , int );
+int _sqrt_recursion(int );
+int is_prime_number(int );
+int is_prime_number2(int , int );
+void myrecursion(char *, int );
+int count(char *, int );
+unsigned int _strspn(char *, char *);
+int _atoi(char *);
+int _islower(int );
+int _isalpha(int );
+int _abs(int );
+int _isupper(int c);
+int _isdigit(int c);
+char *_strcpy(char *, char *);
+char *_strncat(char *, char *, int );
+char *_strcat(char *, char *);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *_memset(char *, char , unsigned int );
+char *_memcpy(char *, char *, unsigned int);
+int _atoi(char *s);
+void _puts(char *);
+
+#endif /*HOLBERTON_H*/
