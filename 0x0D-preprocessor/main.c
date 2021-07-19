c3eddd2b4439fa868ee5c54eb322753b2a479abc
9jazeez <9jazeez@gmail.com>
2021-07-19 08:53:46 +0000

    Task on 0x0D-pre completed

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,18 @@
+#include "4-sum.h"
+#include "4-sum.h"
+#include "3-function_like_macro.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int r;
+	int c;
+
+	r = ABS(-98) * 10;
+	a = 98 * 10;
+	c = ABS(2);
+	printf("%d , %d , %d\n", SUM(c, r), a, c);
+
+	return (0);
+}
