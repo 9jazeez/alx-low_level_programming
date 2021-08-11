78f91759ad5338c348f3c684f168ded89a954b4e
9jazeez <9jazeez@gmail.com>
2021-08-11 21:12:39 +0000

    NULL value in task 1 corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t e;
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
-	if (fd == -1 || filename == NULL)
+	if (fd == -1 || filename == NULL || text_content == NULL)
 	{
 		return (-1);
 	}
